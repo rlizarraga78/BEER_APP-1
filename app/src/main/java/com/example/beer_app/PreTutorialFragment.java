13dea1fd1f6a94958edcc5a288e4eb7b4c886e09
DFerre13 <devinferre@gmail.com>
2020-05-01 19:21:58 -0700

    Added tutorial and pretutorial fragments

--- app/src/main/java/com/example/beer_app/PreTutorialFragment.java
+++ app/src/main/java/com/example/beer_app/PreTutorialFragment.java
@@ -1,4 +1,24 @@
 package com.example.beer_app;
 
-public class PreTutorialFragment {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class PreTutorialFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pretutorial_fragment, container, false);
+
+        //Button goToTutorial = (Button) findViewById(R.id.toTutorialButton);
+        //Intent intent = new Intent(PreTutorialFragment.this, TutorialFragment.class);
+        //startActivity(intent);
+        //This doesn't work inside fragments and I'm not sure what needs to be changed -Devin
+    }
 }
