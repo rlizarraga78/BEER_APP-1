38b79de74e3a3cd7d8c276d2cfd36a8456db1aa5
DFerre13 <devinferre@gmail.com>
2020-05-03 14:20:07 -0700

    Create PretutorialFragment.java

---
+++ app/src/main/java/com/example/beer_app/PretutorialFragment.java
@@ -0,0 +1,32 @@
+package com.example.beer_app;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class PreTutorialFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pretutorial_fragment, container, false);
+
+        //Button goToTutorial = (Button) findViewById(R.id.toTutorialButton);
+        //Intent intent = new Intent(PreTutorialFragment.this, TutorialFragment.class);
+        //startActivity(intent);
+
+        //This doesn't work inside fragments and I'm not sure what needs to be changed -Devin
+    }
+}
