5ca6a4054715d172b021224f01a96e604c0bedfb
DFerre13 <devinferre@gmail.com>
2020-05-03 22:49:06 -0700

    Created Transitions between each fragment

--- app/src/main/java/com/example/beer_app/CreateNewFragment.java
+++ app/src/main/java/com/example/beer_app/CreateNewFragment.java
@@ -3,19 +3,44 @@ package com.example.beer_app;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.beer_app.R;
 
 public class CreateNewFragment extends Fragment {
-
+    Button startPreTutorial;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_create_new, container, false);
-    }
+        View v = inflater.inflate(R.layout.activity_create_new, container, false);
+        startPreTutorial =(Button)v.findViewById(R.id.Start_Tutorial_Button);
+
+        startPreTutorial.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new PreTutorialFragment();
+                FragmentManager fragmentManager =getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.CreateNew, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return v;
+    /* Need something like this to replace the fragment
+
+        getFragmentManager().beginTransaction();
+
+        transaction.replace(R.id.CreateNew, PreTutorialFragment);
+        transaction.commit();
+        */}
 }
--- app/src/main/java/com/example/beer_app/PreTutorialFragment.java
+++ app/src/main/java/com/example/beer_app/PreTutorialFragment.java
@@ -5,15 +5,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class PreTutorialFragment extends Fragment {
     @Nullable
-    @Override
+
+    Button startTutorial;
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pretutorial_fragment, container, false);
+        View v = inflater.inflate(R.layout.pretutorial_fragment, container, false);
+
+        startTutorial =(Button)v.findViewById(R.id.toTutorialButton);
+
+        startTutorial.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment fragment = new BrewingTutorialFragment();
+                FragmentManager fragmentManager =getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction =fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.PreTu, fragment);
+                fragmentTransaction.addToBackStack(null);
+                fragmentTransaction.commit();
+            }
+        });
+
+        return v;
     }
 }
